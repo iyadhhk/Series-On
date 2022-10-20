5f616b81a84efed766856e6be97cd32238b203a2
iyadh hakiri <iyadhbox@gmail.com>
2022-10-20 20:50:13 +0100

    add getSeries query to tmdb services file

--- src/services/tmdb.js
+++ src/services/tmdb.js
@@ -10,7 +10,25 @@ export const tmdbApi = createApi({
     getGenres: builder.query({
       query: () => `genre/tv/list?api_key=${tmdbApiKey}`,
     }),
+    // Get series by category and genre id
+    getSeries: builder.query({
+      query: ({ currentCategory, page, searchQuery }) => {
+        // Get Movies by Search
+        if (searchQuery) {
+          return `/search/tv?query=${searchQuery}&page=${page}&api_key=${tmdbApiKey}&include_adult=false`;
+        }
+        // Get Movies by category name
+        if (currentCategory && typeof currentCategory === 'string') {
+          if (currentCategory === 'trending') {
+            return `trending/tv/week?api_key=${tmdbApiKey}`;
+          }
+          return `tv/${currentCategory}?api_key=${tmdbApiKey}&page=1`;
+        }
+        // Get Movies by genre id
+        return `discover/tv?with_genres=${currentCategory}&page=${page}&api_key=${tmdbApiKey}`;
+      },
+    }),
   }),
 });
 
-export const { useGetGenresQuery } = tmdbApi;
+export const { useGetGenresQuery, useGetSeriesQuery } = tmdbApi;
