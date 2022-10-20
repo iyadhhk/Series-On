28629688c36da123e6821d80c6cf3e96709f1ce7
iyadh hakiri <iyadhbox@gmail.com>
2022-10-20 06:16:09 +0100

    add navigation and home routes

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Navigation from './routes/navigation/navigation.component';
+import Home from './routes/home/home.component';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
